ffafcb4a0e64695ebd24acba1a63725fea93cddd
unknown <nating@tcd.ie>
2021-12-12 22:47:02 +0000

    working on fixing play trigger

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 
 
 var roll = function(){
-  document.getElementById("overlay").classList.add('hidden');
-  document.getElementById('iframe').src = document.getElementById('iframe').dataset.src + '?autoplay=1';
+  document.getElementById('overlay').classList.add('hidden');
+  document.getElementById('iframe').src+='&autoplay=1';
 }
