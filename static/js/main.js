4d33ba11402cb65dd3ed48fb95c80108549c79d6
unknown <nating@tcd.ie>
2021-12-12 22:57:29 +0000

    Testing play triggers

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,5 @@
 
 var roll = function(){
   document.getElementById('overlay').classList.add('hidden');
-  document.getElementById('iframe').src+='&autoplay=1';
+  document.getElementById('iframe').src += '&autoplay=1';
 }
