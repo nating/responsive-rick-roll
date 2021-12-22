5e598f52062c4abbdcbbea2d1e2b6f85d42d6838
Geoffrey Natin <nating@tcd.ie>
2021-12-22 14:50:08 +0000

    Working on desktop and mobile with sound.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 
+document.getElementById('btn').onclick = e => {
+  document.getElementById('vid').play();
+  document.getElementById('overlay').classList.add('hidden');
+}
 
+/*
 var roll = function(){
   document.getElementById('overlay').classList.add('hidden');
   document.getElementById('iframe').src += '&autoplay=1';
 }
+*/
