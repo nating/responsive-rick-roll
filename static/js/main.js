1914e4fa4705abe7ad54b3d650871e37136cf62b
Geoffrey Natin <nating@tcd.ie>
2021-12-22 17:35:14 +0000

    Cleaning up JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,26 @@
 
-const button = document.getElementById('button');
-const overlay = document.getElementById('overlay');
-
-// Load the YouTube iframe API the recommended way: https://stackoverflow.com/a/37284248/7852784
 const tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
+tag.src = 'https://www.youtube.com/iframe_api';
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-let player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video', {
+  new YT.Player('video', {
     videoId: 'dQw4w9WgXcQ',
     playerVars: {
-      'controls': 0,
-      'loop': 1,
-      'modestbranding': 1
+      controls: 0,
+      loop: 1,
+      modestbranding: 1,
     },
     events: {
-      'onReady': onPlayerReady
+      'onReady': (event) => {
+        const button = document.getElementById('button');
+        const overlay = document.getElementById('overlay');
+        button.onclick = () => {
+          event.target.playVideo();
+          overlay.classList.add('hidden');
+        }
+      }
     }
   });
-}
-
-function onPlayerReady(event) {
-  button.onclick = e => {
-    event.target.playVideo();
-    overlay.classList.add('hidden');
-  }
 }
