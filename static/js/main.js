b212a33f1f9440949de8ff7b76af0e6bbffc4930
Geoffrey Natin <nating@tcd.ie>
2021-12-22 17:02:13 +0000

    Removing logs

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,27 @@
 
-const RICK_MP3 = 'https://archive.org/download/rick-astley-never-gonna-give-you-up/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up.mp3';
-const audio =new Audio(RICK_MP3);
 const button = document.getElementById('button');
 const overlay = document.getElementById('overlay');
 
 // Load the YouTube iframe API the recommended way: https://stackoverflow.com/a/37284248/7852784
-var tag = document.createElement('script');
+const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  var player;
-  function onYouTubeIframeAPIReady() {
-    	console.log('0');
-    player = new YT.Player('video', {
-      width: '100%',
-      videoId: 'dQw4w9WgXcQ',
-      playerVars: { 'controls': 0, 'info': 0 },
-      events: {
-        'onReady': onPlayerReady
-      }
-    });
-  }
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('video', {
+    videoId: 'dQw4w9WgXcQ',
+    playerVars: { 'controls': 0, 'info': 0 },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
 
-    function onPlayerReady(event) {
-    	console.log('1');
-     button.onclick = e => {
-    	console.log('2');
-        event.target.playVideo();
-    	console.log('3');
-  overlay.classList.add('hidden');
-    	console.log('4');
-     }
+function onPlayerReady(event) {
+  button.onclick = e => {
+    event.target.playVideo();
+    overlay.classList.add('hidden');
   }
+}
